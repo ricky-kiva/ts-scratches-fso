b8bb6efa3f2c36bf0ec6f5f23a4e5146dc64ccfd
ricky-kiva <ricky.kiva@gmail.com>
2025-02-08 13:45:37 +0700

    feat(index): add endpoint to do calculation

--- index.ts
+++ index.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import { calculator, Operation } from './calculator';
 
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (!value1 || !value2 || !op) {
+    res.status(400).send({ error: 'Insufficient parameter' });
+    return;
+  }
+
+  try {
+    const result = calculator(Number(value1), Number(value2), op as Operation);
+    res.send({ result });
+  } catch(e: unknown) {
+    res.status(400).send({ error: 'Something\'s wrong with the given parameter'});
+    if (e instanceof Error) console.log(`Caught an error: ${e}`);
+  }
 });
 
 const PORT = 3003;
